4e6c4f61c488ee66fe0a736d3ba371b6ead2b412
alekseenkos5660 <alekseenkoredmi4a@gmail.com>
2023-06-04 19:34:22 +0300

    Added the solution of the first task

--- hometask3.py
+++ hometask3.py
@@ -2,6 +2,29 @@
 # экран названия дня недели. Например, если 1, то на экране надпись понедельник, 2 — вторник
 # и т.д.
 
+try:
+    week_day = int(input("Enter the number of the day of the week: "))
+    if week_day == 1:
+        print("\tYou have chosen the first day of the week and it is Monday")
+    elif week_day == 2:
+        print("\tYou have chosen the second day of the week and it is Tuesday")
+    elif week_day == 3:
+        print("\tYou have chosen the third day of the week and it is Wednesday")
+    elif week_day == 4:
+        print("\tYou have chosen the fourth day of the week and it is Tuesday")
+    elif week_day == 5:
+        print("\tYou have chosen the fifth day of the week and it is Friday")
+    elif week_day == 6:
+        print("\tYou have chosen the sixth day of the week and it is Saturday")
+    elif week_day == 7:
+        print("\tYou have chosen the seventh day of the week and it is Sunday")
+    else:
+        raise Exception("\tPlease select a number from 1 to 7")
+except ValueError as error:
+    print("\tEnter only integer numbers please!")
+except Exception as error:
+    print(f"\tError: {error}")
+
 # 2. Пользователь вводит два числа. Определить, равны ли эти числа, и, если нет, вывести их
 # на экран в порядке возрастания
 
